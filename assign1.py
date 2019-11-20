e292c9d49ff91077c0a85ded35afc21eed7c58ca
layacheadeth <layacheadeth@gmail.com>
2019-11-20 12:10:19 +0700

    second comments

--- assign1.py
+++ assign1.py
@@ -1,4 +1,5 @@
 angle= float(input("input the angle in degree: "))
-radian= angle*3.14 /180
+def radian(angle):
+    return (angle*3.14 /180)
 print("angle in degreee: ",angle)
-print("angle in radius: ",radian)
+print("angle in radius: ",radian(angle))
